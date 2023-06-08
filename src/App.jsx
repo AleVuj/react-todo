62dbc85b8f4e4881c6cd485ffb0d1e749defa949
Alex <numerologija369@gmail.com>
2023-06-08 03:01:57 +0200

    commented

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   const [todos, setTodos] = useState(() => {
     const localValue = localStorage.getItem("ITEMS")
     if (localValue == null) return []
-//provera da li je null
+//provera da li je nulliran
     return JSON.parse(localValue)
   })
 
